d2e7cecaa37e3461632ba86478b4780cf9481c0b
Ali448 <33634855+Ali448@users.noreply.github.com>
2018-02-01 23:18:07 +0500

    git learning 1

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
+const dotnenv = require('dotenv');
+         dotnenv.config();           
+
 console.log('This is my very very and very complex application');
 console.log('Very big application 20000 lines of code');
