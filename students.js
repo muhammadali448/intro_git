861c942f185cd8724c7a2e0f21db057db6de8c64
Ali448 <33634855+Ali448@users.noreply.github.com>
2018-02-01 20:54:45 +0500

    Maryam is added to BCE-4 semester

--- students.js
+++ students.js
@@ -1,4 +1,4 @@
-const students = ['Muhammad Ali', 'Yasir', 'Talha', 'Taha', 'Raja', 'Eraj', 'Amna'];
+const students = ['Muhammad Ali', 'Yasir', 'Talha', 'Taha', 'Raja', 'Eraj', 'Amna', 'Maryam'];
 
 for (const student of students) {
     console.log(student);
