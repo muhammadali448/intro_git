10983e9844c99df97c999dc6c95ede7e2581db45
Ali448 <33634855+Ali448@users.noreply.github.com>
2018-02-01 10:14:02 +0500

    update app and human files

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
 console.log('This is my very very and very complex application');
+console.log('Very big application 20000 lines of code');
--- human.js
+++ human.js
@@ -1,1 +1,2 @@
 console.log('I am a human, but you are not hahahahaha');
+console.log('Talha and Eraj are also humans');
