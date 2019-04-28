8ff9eef81baf8132feb328f673e1d42f34816c76
muhammadali448 <muhammadali19960@outlook.com>
2019-04-28 23:11:34 +0500

    modified human.js in new branch

--- human.js
+++ human.js
@@ -1,3 +1,4 @@
 console.log('I am a human, but you are not hahahahaha');
 console.log('Talha and Eraj are also humans');
 console.log('YASIR AND RAJA HAVE A HUSBAND WIFE RELATION.. DONT TELL ANYONE')
+console.log('ALI AND TAHA ARE SINGLE MATERIAL')
