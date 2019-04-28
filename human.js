a9e76021d44eb2aad7e973e7b7c31ffec3108821
muhammadali448 <muhammadali19960@outlook.com>
2019-04-28 23:18:35 +0500

    append some text in human.js

--- human.js
+++ human.js
@@ -2,3 +2,4 @@ console.log('I am a human, but you are not hahahahaha');
 console.log('Talha and Eraj are also humans');
 console.log('YASIR AND RAJA HAVE A HUSBAND WIFE RELATION.. DONT TELL ANYONE')
 console.log('ALI AND TAHA ARE SINGLE MATERIAL')
+console.log('DANIYAL AND HASSAN ARE BF AND GF FOREVER APPENDED')
