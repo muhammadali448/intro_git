552278f7037d044596590be2b7fb5323a1c43cd1
Muhammad Ali <muhammadali19960@outlook.com>
2019-04-29 10:30:27 +0500

    remote: student.js

--- students.js
+++ students.js
@@ -1,4 +1,4 @@
-const students = ['Muhammad Ali', 'Yasir', 'Talha', 'Taha', 'Raja', 'Eraj', 'Amna'];
+const students = ['Muhammad Ali', 'Yasir', 'Talha', 'Taha', 'Raja', 'Eraj', 'Amna', 'Maryam', 'Nuzhat'];
 
 for (const student of students) {
     console.log(student);
