a9905fa76e71251b3734c7ff8d2ea8ea1b015bfd
muhammadali448 <muhammadali19960@outlook.com>
2019-04-29 10:59:55 +0500

    after pop stash

--- students.js
+++ students.js
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 const students = ['Muhammad Ali', 'Yasir', 'Talha', 'Taha', 'Raja', 'Eraj', 'Amna', 'Maryam', 'Nuzhat', 'Tania', 'Daniyal'];
+=======
+const students = ['Muhammad Ali', 'Yasir', 'Talha', 'Taha', 'Raja', 'Eraj', 'Amna', 'Maryam', 'Nuzhat', 'Tania', 'Laraib'];
+>>>>>>> Stashed changes
 
 for (const student of students) {
     console.log(student);
