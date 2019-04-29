eec4980ae76a08dea5113cdb1cad3e3eee079c65
muhammadali448 <muhammadali19960@outlook.com>
2019-04-29 10:44:31 +0500

    students.js file updated

--- students.js
+++ students.js
@@ -1,4 +1,4 @@
-const students = ['Muhammad Ali', 'Yasir', 'Talha', 'Taha', 'Raja', 'Eraj', 'Amna', 'Maryam', 'Nuzhat'];
+const students = ['Muhammad Ali', 'Yasir', 'Talha', 'Taha', 'Raja', 'Eraj', 'Amna', 'Maryam', 'Nuzhat', 'Tania'];
 
 for (const student of students) {
     console.log(student);
