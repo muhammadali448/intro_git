b44823ca82a9499aec5d08aa7a52efa48f4d1ea6
muhammadali448 <muhammadali19960@outlook.com>
2019-04-29 11:22:56 +0500

    changes in students.js

--- students.js
+++ students.js
@@ -1,8 +1,6 @@
-<<<<<<< Updated upstream
-const students = ['Muhammad Ali', 'Yasir', 'Talha', 'Taha', 'Raja', 'Eraj', 'Amna', 'Maryam', 'Nuzhat', 'Tania', 'Daniyal'];
-=======
-const students = ['Muhammad Ali', 'Yasir', 'Talha', 'Taha', 'Raja', 'Eraj', 'Amna', 'Maryam', 'Nuzhat', 'Tania', 'Laraib'];
->>>>>>> Stashed changes
+
+const students = ['Muhammad Ali', 'Yasir', 'Talha', 'Taha', 'Raja', 'Eraj', 'Amna', 'Maryam', 'Nuzhat', 'Tania', 'Daniyal', 'Ali'];
+
 
 for (const student of students) {
     console.log(student);
